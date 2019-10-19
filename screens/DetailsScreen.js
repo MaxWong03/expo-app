ccc6e9ceb8e434541219be6738bb71183211ac81
Rindish Krishna <rkatonline@gmail.com>
2019-10-19 19:19:41 +0530

    converted to class based components

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -10,11 +10,12 @@ import {
   View,
 } from 'react-native';
 
-export default function DetailsScreen() {
+export default class Details extends Components {
+  render(){
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Details!</Text>
     </View>
   );
 }
-
+}
